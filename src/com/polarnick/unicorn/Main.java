7548e758f49a1cb2f532b8fc85f315cf8610f477
Nickolay Polyarniy <PolarHare@gmail.com>
2014-06-17 23:18:42 +0400

    Ant build file created.

---
+++ src/com/polarnick/unicorn/Main.java
@@ -0,0 +1,12 @@
+package com.polarnick.unicorn;
+
+/**
+ * @author Nickolay Polyarnyi
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
